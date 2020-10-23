ef650fbfca11d937b1516e05a1971675bb8c5d92
psnoya <paulina.s.noya@gmail.com>
2020-10-23 04:54:45 +0200

    Created a button

--- cypress/integration/userCanAddItems.feature.js
+++ cypress/integration/userCanAddItems.feature.js
@@ -2,7 +2,7 @@ describe('user can add items', () => {
     it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-item').click()
-        cy.get('#notes').type('Dont forget anything on the list')
+        cy.get('#notes').type('Do not forget anything on the list')
         cy.get('#submit').click()
     })
 })
